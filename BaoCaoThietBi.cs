4ca70f7e3eb1a2d0b7c23a22c1897c78bb68b68f
Nhat Anh <vipfo123@gmail.com>
2021-11-22 15:54:12 +0700

    Add BaoCaoThietBi class Remove HoaDonDichVu class

---
+++ BaoCaoThietBi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_QuanLyKTX
+{
+    //Báo cáo hỏng hóc thiết bị(Mã báo cáo, Mã phòng, Thời gian, Mô tả)
+    public class BaoCaoThietBi
+    {
+        public string maBaoCao { get; set; }
+        public Phong phong { get; set; }
+        public DateTime thoiGian { get; set; }
+        public string moTaBaoCao { get; set; }
+        
+        public BaoCaoThietBi(string maBaoCao, Phong phong, DateTime thoiGian, string moTaBaoCao)
+        {
+            this.maBaoCao = maBaoCao;
+            this.phong = phong;
+            this.thoiGian = thoiGian;
+            this.moTaBaoCao = moTaBaoCao;
+        }
+        public BaoCaoThietBi(BaoCaoThietBi baoCao)
+        {
+            maBaoCao = baoCao.maBaoCao;
+            phong = baoCao.phong;
+            thoiGian = baoCao.thoiGian;
+            moTaBaoCao = baoCao.moTaBaoCao;
+        }
+    }
+}
